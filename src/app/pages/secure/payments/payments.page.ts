e3e32008dc1d5d9b8af65e170eebfad85304e7b3
DESKTOP-F0HPPO1\USER <revathipriyaredrouthu@gmail.com>
2026-06-18 12:15:22 +0530

    Upgrade Ionic from v6 to v8

--- src/app/pages/secure/payments/payments.page.ts
+++ src/app/pages/secure/payments/payments.page.ts
@@ -30,10 +30,8 @@ export class PaymentsPage implements OnInit {
 
     // Open filter modal
     const modal = await this.modalController.create({
-      component: FilterPage,
-      swipeToClose: true,
-      presentingElement: this.routerOutlet.nativeEl
-    });
+  component: FilterPage
+});
 
     await modal.present();
 
